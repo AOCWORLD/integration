4241e741003ea921b66f0d27716bec57e8f9378c
unknown <Administrator@DEV10-PC.sifast.lan>
2014-08-07 19:01:39 +0200

    header home page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+	
+	//toggle menu langue
+	$('.dropdown-toggle').click(function(){
+		if($(this).parent('.dropdown').hasClass('open')) {
+			$(this).parent('.dropdown').removeClass('open');
+			$(this).parent('.dropdown').find('.dropdown-body').hide();
+		}else {
+			$(this).parent('.dropdown').addClass('open');
+			$(this).parent('.dropdown').find('.dropdown-body').show();
+		}
+	});
 
 }); //ready
