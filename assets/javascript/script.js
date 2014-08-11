806b33439a236f7bf1ce028215e5f8d05345e773
unknown <Administrator@DEV10-PC.sifast.lan>
2014-08-11 15:28:57 +0200

    carousel

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	
-	//toggle menu langue
+	//toggle menu
 	$('.dropdown-toggle').click(function(){
 		if($(this).parent('.dropdown').hasClass('open')) {
 			$(this).parent('.dropdown').removeClass('open');
@@ -9,6 +9,30 @@ $(document).ready(function() {
 			$(this).parent('.dropdown').addClass('open');
 			$(this).parent('.dropdown').find('.dropdown-body').show();
 		}
+	});
+
+	//carousel
+	$('#myCarousel').carousel({
+	    interval: 4000
+	});
+
+	// handles the carousel thumbnails
+	$('[id^=carousel-selector-]').click( function(){
+	  var id_selector = $(this).attr("id");
+	  var id = id_selector.substr(id_selector.length -1);
+	  id = parseInt(id);
+	  $('#myCarousel').carousel(id);
+	  $('[id^=carousel-selector-]').removeClass('selected');
+	  $(this).addClass('selected');
+	});
+
+	// when the carousel slides, auto update
+	$('#myCarousel').on('slid', function (e) {
+	  var id = $('.item.active').data('slide-number');
+	  var id_selector = $(this).attr("id");
+	  id = parseInt(id);
+	  $('[id^=carousel-selector-]').removeClass('selected');
+	  $('[id^=carousel-selector-'+id+']').addClass('selected');
 	});
 
 }); //ready
